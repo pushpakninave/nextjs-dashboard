b01eccaea25568eb78091f393cb04a1690b1face
pushpakninave <pushpakninave.jee@gmail.com>
2024-08-16 19:12:52 +0530

    done with authentication

--- app/ui/dashboard/sidenav.tsx
+++ app/ui/dashboard/sidenav.tsx
@@ -3,6 +3,7 @@ import NavLinks from '@/app/ui/dashboard/nav-links';
 import AcmeLogo from '@/app/ui/acme-logo';
 import { PowerIcon } from '@heroicons/react/24/outline';
 import { ModeToggle } from '@/components/theme-toggle';
+import { signOut } from '@/auth';
 
 export default function SideNav() {
   return (
@@ -16,12 +17,17 @@ export default function SideNav() {
         </div>
       </Link>
       <div>
-      <ModeToggle />
+        <ModeToggle />
       </div>
       <div className="flex grow flex-row justify-between space-x-2 md:flex-col md:space-x-0 md:space-y-2">
         <NavLinks />
         <div className="dark:bg-slate-800 hidden h-auto w-full grow rounded-md bg-gray-50 md:block"></div>
-        <form>
+        <form action={async () => {
+          'use server';
+          console.log('am i calling it ?');
+          
+          await signOut();
+        }}>
           <button className="dark:bg-cyan-700 flex h-[48px] w-full grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 dark:hover:text-black md:flex-none md:justify-start md:p-2 md:px-3">
             <PowerIcon className="w-6" />
             <div className="hidden md:block">Sign Out</div>
